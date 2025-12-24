114574887581d660a384b71b6e8e9fe99ed5108a
shubham-Alhat <shubhamalhat520@gmail.com>
2025-12-24 22:00:29 +0530

    feat: add websocket connection setup in Chat component and create SOCKET.md for documentation

---
+++ SOCKET.md
@@ -0,0 +1,7 @@
+## Websocket API (browser object)
+
+### While reading mdn websocket api docs. found note about `backpressure`.
+
+Go on mdn docs read the problems and refer below claude chat.
+
+[claude chat about backpressure](https://claude.ai/share/83e4d9d9-5a69-4742-9509-9209c64c62da)
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,8 +1,27 @@
 "use client";
 
 import { ChatLayout } from "@/components/chat-layout";
+import { Button } from "@/components/ui/button";
+import { useEffect, useRef } from "react";
 
 function Chat() {
+  const websocketRef = useRef<WebSocket>(null);
+
+  useEffect(() => {
+    if (websocketRef.current) {
+      console.log("already socket..");
+      return;
+    }
+
+    // console.log("check..");
+    const websocket = new WebSocket("ws://localhost:8000/ws");
+    websocketRef.current = websocket;
+    websocket.addEventListener("open", () => {
+      console.log("connected to ws server..");
+      websocket.send("hey.. from frontend!!");
+    });
+  }, []);
+
   return (
     <>
       <div>
