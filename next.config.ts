3bf6bc39bbc948069e1c2ceba2a4c1a7ef2c3fd9
shubham-Alhat <shubhamalhat520@gmail.com>
2025-12-30 16:13:58 +0530

    done 01

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "https://chattie-server.onrender.com/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
